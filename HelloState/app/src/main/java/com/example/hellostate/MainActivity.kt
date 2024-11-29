b058a356543225514d4ba957020e2b5136816955
= <=>
2024-11-29 13:44:14 +0100

    save

--- HelloState/app/src/main/java/com/example/hellostate/MainActivity.kt
+++ HelloState/app/src/main/java/com/example/hellostate/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.hellostate
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.hellostate.ui.theme.HelloStateTheme
@@ -29,13 +35,15 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    var count = mutableStateOf(0)
+    Button(onClick = { count++ }) {
+        Text(text = "Hello, $name! You've clicked $count times")
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
